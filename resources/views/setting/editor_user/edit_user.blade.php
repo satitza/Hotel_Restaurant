e66b771f1c99e29dd691590121e108f270b90539
Satit Prontepanon <st_satitza@hotmail.com>
2018-03-19 14:49:52 +0700

    commit
    commit

---
+++ resources/views/setting/editor_user/edit_user.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">Edit Editor User</div>
+
+                    <div class="panel-body">
+
+                        <div class="form-group">
+                            <?php // {!! Form::open(['url' => 'setting/report/users', 'files' => false]) !!} ?>
+                            {{ Form::open(array('url' => 'setting/editor/users/'.$id, 'method' => 'put')) }}
+                            <table class="table table-striped table-hover ">
+                                <thead>
+                                <tr class="">
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>{{ Form::label('lb_user_report', 'ชื่อ') }}</td>
+                                    <td>
+                                        <div class="form-group">
+                                            <select class="form-control" name="user_id">
+                                                <!--option value="" disabled selected>please_selected</option-->
+                                                <option value="{{ $user_id }}">{{ $user_name }}</option>
+                                            </select>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>{{ Form::label('lb_old_restaurant', 'ร้านอาหารที่ Match แล้ว') }}</td>
+                                    <td>
+                                        @foreach($old_restaurants as $indexKey => $old_restaurant )
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" name="old_restaurants_check_box[]"
+                                                           value="{{ $old_restaurants_id[$indexKey] }}" checked> {{ $old_restaurant }}
+                                                </label>
+                                            </div>
+                                        @endforeach
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>{{ Form::label('lb_restaurant', 'ร้านอาหาร') }}</td>
+                                    <td>
+
+                                        <?php /* {{ Form::checkbox('role', $restaurant->id, in_array($restaurant->id, $all_data[])) }} ?>
+                                            {{ Form::label('role', $restaurant->restaurant_name) }}<br>  */ ?>
+                                        <div class="checkbox">
+                                            <label>
+                                                @foreach($restaurants as $restaurant)
+                                                <input type="checkbox" name="restaurants_check_box[]" value="">
+                                            </label>
+                                        </div>
+
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <center>
+                                {{ Form::submit('Match User', ['class' => 'btn btn-primary']) }}
+                            </center>
+                            {{ csrf_field() }}
+                            {!! Form::close() !!}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
