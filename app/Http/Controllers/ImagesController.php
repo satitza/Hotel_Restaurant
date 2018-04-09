4f12436f832507384bd4fd7671a1467d08711ec0
Satit Prontepanon <st_satitza@hotmail.com>
2018-04-09 15:27:13 +0700

    commit
    commit

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use DB;
-use App\Image;
+use App\Images;
 use App\Offers;
 use File;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use League\Flysystem\Exception;
-//use Intervention\Image\ImageManagerStatic as Image;
+use Intervention\Image\ImageManagerStatic as Image;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ImagesController extends Controller
@@ -66,7 +66,6 @@ class ImagesController extends Controller
 
             }
 
-
             reset($images);
 
             return view('image.list', [
@@ -91,9 +90,9 @@ class ImagesController extends Controller
             $collect = array();
 
             //Get old images from table images
-            if (Image::where('offer_id', '=', $request->offer_id)->exists()) {
+            if (Images::where('offer_id', '=', $request->offer_id)->exists()) {
 
-                $old_images = Image::select('image')->where('offer_id', $request->offer_id)->get();
+                $old_images = Images::select('image')->where('offer_id', $request->offer_id)->get();
                 foreach ($old_images as $old_image) {
                     if ($old_image->image != "") {
                         array_push($collect, $old_image->image);
@@ -104,25 +103,29 @@ class ImagesController extends Controller
             if ($files = $request->file('images')) {
                 try {
                     foreach ($files as $file) {
+
                         $name = rand() . "." . $file->getClientOriginalExtension();
+                        $new_file_name = rand() . "." . $file->getClientOriginalExtension();
+
                         $destinationPath = public_path('/images');
 
-                        //$image_resize = Image::make($name->getRealPath());
-                        //$image_resize->resize(400, 800);
-                        //$image_resize->save($destinationPath, $name);
                         $file->move($destinationPath, $name);
-                        $images[] = $name;
-                        array_push($collect, $name);
 
+                        Image::make($destinationPath . '/' . $name)->resize(800, 400)->save($destinationPath . '/' . $new_file_name);
+                        File::delete(public_path('images\\' . $name));
+
+                        $images[] = $new_file_name;
+                        array_push($collect, $new_file_name);
                     }
                 } catch (FileException $e) {
                     return view('error.index')->with('error', $e);
                 }
             }
 
+
             DB::beginTransaction();
             try {
-                if (Image::where('offer_id', '=', $request->offer_id)->exists()) {
+                if (Images::where('offer_id', '=', $request->offer_id)->exists()) {
 
                     DB::table('images')
                         ->where('offer_id', $request->offer_id)
@@ -133,7 +136,7 @@ class ImagesController extends Controller
                     DB::commit();
                     return redirect()->action('ImagesController@create');
                 } else {
-                    $images = new Image;
+                    $images = new Images;
                     $images->offer_id = $request->offer_id;
                     $images->image = implode(",", $collect);
                     $images->save();
@@ -209,7 +212,7 @@ class ImagesController extends Controller
     public function UnsetItem($id, array $items)
     {
         try {
-            $old_images = Image::find($id);
+            $old_images = Images::find($id);
             $old_images_array = explode(',', $old_images->image);
             reset($old_images_array);
 
@@ -301,7 +304,7 @@ class ImagesController extends Controller
     public function DeleteAllImage($id)
     {
         try {
-            $old_images = Image::find($id);
+            $old_images = Images::find($id);
             $old_images_array = explode(',', $old_images->image);
             reset($old_images_array);
 
