37ad051363c285dde13d84b1c80db68eb24de0a0
Satit Prontepanon <st_satitza@hotmail.com>
2018-05-31 17:57:33 +0700

    commit
    commit

--- app/Http/Controllers/TermsController.php
+++ app/Http/Controllers/TermsController.php
@@ -38,34 +38,58 @@ class TermsController extends Controller
 
     public function create($id)
     {
-          return view('terms.list');
+        try {
+
+            $offers = Offers::select('id', 'offer_name_en')->where('id', '=', $id)->first();
+            $terms_th = Termsth::where('offer_id', '=', $id)->paginate(10);
+            $terms_en = Termsen::where('offer_id', '=', $id)->paginate(10);
+            $terms_cn = Termscn::where('offer_id', '=', $id)->paginate(10);
+
+            return view('terms.list', [
+                'offer_id' => $offers->id,
+                'offer_name' => $offers->offer_name_en,
+                'terms_th' => $terms_th,
+                'terms_en' => $terms_en,
+                'terms_cn' => $terms_cn
+            ]);
+
+        } catch (QueryException $e) {
+            return view('error.index')->with('error', $e->getMessage());
+        } catch (Exception $e) {
+            return view('error.index')->with('error', $e->getMessage());
+        }
     }
 
     public function store(Request $request)
     {
         try {
 
-            $terms_th = new Termsth;
-            $terms_th->offer_id = $request->offer_id;
-            $terms_th->term_header_th = $request->term_header_th;
-            $terms_th->term_content_th = $request->term_content_th;
-            $terms_th->save();
+            if ($request->term_header_th != null && $request->term_content_th != null) {
+                $terms_th = new Termsth;
+                $terms_th->offer_id = $request->offer_id;
+                $terms_th->term_header_th = $request->term_header_th;
+                $terms_th->term_content_th = $request->term_content_th;
+                $terms_th->save();
+            }
 
-            $terms_en = new Termsen;
-            $terms_en->offer_id = $request->offer_id;
-            $terms_en->term_header_en = $request->term_header_en;
-            $terms_en->term_content_en = $request->term_content_en;
-            $terms_en->save();
+            if ($request->term_header_en != null && $request->term_content_en != null) {
+                $terms_en = new Termsen;
+                $terms_en->offer_id = $request->offer_id;
+                $terms_en->term_header_en = $request->term_header_en;
+                $terms_en->term_content_en = $request->term_content_en;
+                $terms_en->save();
+            }
 
-            $terms_cn = new Termscn;
-            $terms_cn->offer_id = $request->offer_id;
-            $terms_cn->term_header_cn = $request->term_header_cn;
-            $terms_cn->term_content_cn = $request->term_content_cn;
-            $terms_cn->save();
-
+            if ($request->term_header_cn != null && $request->term_content_cn != null) {
+                $terms_cn = new Termscn;
+                $terms_cn->offer_id = $request->offer_id;
+                $terms_cn->term_header_cn = $request->term_header_cn;
+                $terms_cn->term_content_cn = $request->term_content_cn;
+                $terms_cn->save();
+            }
 
             DB::commit();
-            return redirect()->action('OffersController@create');
+            return redirect()->action('TermsController@create', [$request->offer_id]);
 
         } catch (QueryException $e) {
             return view('error.index')->with('error', $e->getMessage());
--- resources/views/terms/list.blade.php
+++ resources/views/terms/list.blade.php
@@ -43,6 +43,8 @@
                     <div class="panel-heading">Terms & Conditions</div>
                     <div class="panel-body">
 
+                        {{ Form::label('lb_offer_name', 'Offer Name') }}
+                        {{ Form::text('offer_name_en', $offer_name, ['class' => 'form-control', 'placeholder' => 'Offer Name', 'readonly']) }}
 
                         <div class="container">
                             <h3>Terms & Conditions</h3>
@@ -54,59 +56,128 @@
 
                             <div class="tab-content">
                                 <div id="tab_th" class="tab-pane fade in active">
+
                                 <!--{!! Form::open(['url' => '', 'files' => false]) !!} -->
                                     <table class="table">
                                         <thead class="thead-dark">
                                         <tr>
-                                            <th scope="col"></th>
-                                            <th scope="col">Restaurant Name</th>
-                                            <th scope="col">Hotel Name</th>
-                                            <th scope="col">E-Mail</th>
-                                            <th scope="col">Status</th>
-                                            <!--th scope="col">Restaurant Comment</th-->
-                                            <th scope="col1">Edit Restaurant</th>
-                                            <th scope="col1">Delete</th>
+                                            <th scope="col">หัวข้อ</th>
+                                            <th scope="col">รายละเอียด</th>
+                                            <th scope="col1">แก้ใข</th>
+                                            <th scope="col1">ลบ</th>
                                         </tr>
                                         </thead>
-
+                                        @foreach($terms_th as $term_th)
                                         <tr>
-                                            <th></th>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-
+                                            <td>{{ $term_th->term_header_th }}</td>
+                                            <td>{{ $term_th->term_content_th }}</td>
                                             <td>
                                                 <a href="{{ url('restaurant//edit') }}"
                                                    class="button-link-success">
-                                                    Edit Restaurant
+                                                    แก้ใข
                                                 </a>
                                             </td>
                                             <td>
                                                 <a href="{{ url('delete_restaurant/' ) }}"
                                                    class="button-link-dark"
                                                    onclick="return confirm('Confrim Delete ?')">
-                                                    Delete Restaurant
+                                                    ลบ
                                                 </a>
                                             </td>
                                         </tr>
-
+                                        @endforeach
                                         </tbody>
                                     </table>
-                                {{--                    {!! $restaurants->render() !!}--}}
+                                {!! $terms_th->render() !!}
+                                <!--{{ csrf_field() }}
+                                {!! Form::close() !!} -->
+
+
+                                </div>
+                                <div id="tab_en" class="tab-pane fade">
+
+                                <!--{!! Form::open(['url' => '', 'files' => false]) !!} -->
+                                    <table class="table">
+                                        <thead class="thead-dark">
+                                        <tr>
+                                            <th scope="col">Header</th>
+                                            <th scope="col">Content</th>
+                                            <th scope="col1">Edit</th>
+                                            <th scope="col1">Delete</th>
+                                        </tr>
+                                        </thead>
+                                        @foreach($terms_en as $term_en)
+                                        <tr>
+                                            <td>{{ $term_en->term_header_en }}</td>
+                                            <td>{{ $term_en->term_content_en }}</td>
+                                            <td>
+                                                <a href="{{ url('restaurant//edit') }}"
+                                                   class="button-link-success">
+                                                    Edit
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a href="{{ url('delete_restaurant/' ) }}"
+                                                   class="button-link-dark"
+                                                   onclick="return confirm('Confrim Delete ?')">
+                                                    Delete
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                {!! $terms_en->render() !!}
                                 <!--{{ csrf_field() }}
                                 {!! Form::close() !!} -->
 
                                 </div>
-                                <div id="tab_en" class="tab-pane fade">
-                                   {{--table 2--}}
-                                </div>
                                 <div id="tab_cn" class="tab-pane fade">
-                                   {{--table 3--}}
+
+                                <!--{!! Form::open(['url' => '', 'files' => false]) !!} -->
+                                    <table class="table">
+                                        <thead class="thead-dark">
+                                        <tr>
+                                            <th scope="col">頭</th>
+                                            <th scope="col">內容</th>
+                                            <th scope="col1">編輯</th>
+                                            <th scope="col1">刪除</th>
+                                        </tr>
+                                        </thead>
+                                        @foreach($terms_cn as $term_cn)
+                                        <tr>
+                                            <td>{{ $term_cn->term_header_cn }}</td>
+                                            <td>{{ $term_cn->term_content_cn }}</td>
+                                            <td>
+                                                <a href="{{ url('restaurant//edit') }}"
+                                                   class="button-link-success">
+                                                    編輯
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a href="{{ url('delete_restaurant/' ) }}"
+                                                   class="button-link-dark"
+                                                   onclick="return confirm('Confrim Delete ?')">
+                                                    刪除
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                {!! $terms_cn->render() !!}
+                                <!--{{ csrf_field() }}
+                                {!! Form::close() !!} -->
+
                                 </div>
                             </div>
                         </div>
 
+                        <center>
+                            <a href="{{ url('insert_term/'.$offer_id) }}" class="button-link-success">
+                                Add Terms & Conditions
+                            </a>
+                        </center>
 
                     </div>
                 </div>
