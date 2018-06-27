3c5bb464c1aacf6a021ab687274b84ac7d0e99dd
Satit Prontepanon <st_satitza@hotmail.com>
2018-06-27 13:02:11 +0700

    commit
    commit

--- database/seeds/CreateAdministratorAccountSeeder.php
+++ database/seeds/CreateAdministratorAccountSeeder.php
@@ -12,19 +12,14 @@ class CreateAdministratorAccountSeeder extends Seeder
     public function run()
     {
         $users = [
-            ['name' => 'Administrator', 'email' => 'dining@compasshospitality.com', 'password' => bcrypt('dr823c1HEE'), 'role' => 1],
-            ['name' => 'Satit Porntepanon', 'email' => 'st_satitza@hotmail.com', 'password' => bcrypt('dr823c1HEE'), 'role' => 1],
-            ['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => bcrypt('dr823c1HEE'), 'role' => 1],
-            ['name' => 'Editor', 'email' => 'editor@editor.com', 'password' => bcrypt('dr823c1HEE'), 'role' => 2],
-            ['name' => 'Report', 'email' => 'report@report.com', 'password' => bcrypt('dr823c1HEE'), 'role' => 3],
-            ['name' => 'User', 'email' => 'user@user.com', 'password' => bcrypt('dr823c1HEE'), 'role' => 4],
+            ['name' => '555', 'email' => '555', 'password' => bcrypt('555'), 'role' => 1],
         ];
 
         foreach ($users as $use){
             $user = New App\User();
             $user->name = $use['name'];
             $user->email = $use['email'];
-            $user->password = bcrypt('dr823c1HEE');
+            $user->password = $use['password'];
             $user->user_role = $use['role'];
             $user->save();
         }
